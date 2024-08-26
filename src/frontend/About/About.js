e1b296746ed25068b15ca5c7ce800e8578e0fc8c
GSDion <97761993+GSDion@users.noreply.github.com>
2024-08-26 01:39:15 -0400

    Altered About section and homepage
    - Added placeholder paragraph for About section
    - Added photo for about section
    - Altered color of initials on header
    - added portfolio photo to gitignore
    - Removed <hr> from about page and header page

--- src/frontend/About/About.js
+++ src/frontend/About/About.js
@@ -1,17 +1,33 @@
 import Header from "../Header";
 import './About.css';
 //add some animations to the background (rainbow like intro?)
+
 function About() {
     return (
-      <div className =  'About'>
+      <div className =  'About Dion'>
 
-        <div className = 'about_title '>
+        <div className = 'About_title '>
         <h1>About</h1>
        </div>
 
-        <div className = 'about_paragraph'> </div>
+      <div className = 'About_container'>
+          <div className = 'About_paragraph'> 
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fringilla mattis ligula. Cras facilisis, lectus in laoreet pellentesque, nisl odio aliquet elit, a sagittis metus nisi eu ex. Nullam justo quam, convallis vitae est sit amet, facilisis elementum eros. In in tristique lorem, ac facilisis sapien. Nulla sed metus convallis, rhoncus neque sed, efficitur elit. Sed sed ligula venenatis, efficitur leo sit amet, euismod sem. Aenean lacus justo, lobortis a nisi non, facilisis suscipit massa. 
+            Mauris faucibus sapien in augue bibendum feugiat. Vivamus vitae risus interdum, malesuada sapien id, blandit lectus. 
+            Sed in nibh ipsum. Ut ut pharetra turpis. Quisque eleifend, nisl eget mattis posuere, libero odio gravida neque, 
+            et elementum neque erat eget nibh. Praesent id laoreet justo. Ut interdum, lectus luctus molestie consectetur, 
+            lacus quam ornare metus, vitae dignissim tortor nunc sed nisl. Phasellus varius justo ante, vitae suscipit nisl pretium vitae. 
+            Phasellus elementum ligula eget enim sagittis finibus. Vestibulum aliquet ipsum sed laoreet blandit. 
+            Ut ipsum diam, tristique id ullamcorper in, eleifend sit amet nisi. 
+            Sed vitae mauris hendrerit, commodo diam in, luctus quam. Mauris at interdum diam. 
+            Pellentesque tincidunt semper nunc, non laoreet lacus.</p>
+          </div>
 
-        <div className = 'about_photo'> </div>
+          <div className = 'About_photo'> 
+            <img src={require('../pictures/DH_portfolio.jpg')} alt="DH_portfolio" />
+          </div>
+      </div>
+        
       </div>
     );
 }  
--- src/frontend/Header.js
+++ src/frontend/Header.js
@@ -1,21 +1,16 @@
-import { Link } from 'react-router-dom';
 import './Header.css';
-import Homepage from './Homepage';
 
 function Header() {
   return (
     <div className="header">
       <h1 className="header_title">D.H.</h1>
       <div className="header_hrefs">
-        {/* <Link to="/About" className="header_link">About</Link>
-        <Link to="/Skills" className="header_link">Skills</Link>
-        <Link to="/Projects" className="header_link">Projects</Link>
-        <Link to="/Contact" className="header_link">Contact</Link> */}
+       
         <a href="#about">About</a>
         <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
-       <hr></hr>
+       {/* <hr></hr> */}
       </div>
     </div>
   );
