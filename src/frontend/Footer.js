adbf3c2bebd316b4276cd3952452c78ef7ebdf0c
GSDion <97761993+GSDion@users.noreply.github.com>
2024-10-21 17:35:43 -0400

    Address npm warnings/errors
    - Added whitespace to Footer_social_links anchors to  prevent "Anchors must have content and the content must be accessible by a screen reader" error (must address later)
    - Ran "npm install --save-dev @babel/plugin-proposal-private-property-in-object" to address babel dependency warnings

--- src/frontend/Footer.js
+++ src/frontend/Footer.js
@@ -9,9 +9,9 @@ function Footer() {
         </div>
 
         <div className="Footer_social_links">
-            <a href="https://www.linkedin.com/in/dion-humphrey/" target='blank' class="fa fa-linkedin"></a>
-            <a href="https://github.com/GSDion" target='blank' class="fa fa-github" ></a>
-            <a href="mailto:humphreydion@yahoo.com?subject=Hello!" target='blank' class="fa fa-yahoo" ></a>
+            <a href="https://www.linkedin.com/in/dion-humphrey/" target='blank' class="fa fa-linkedin"> </a>
+            <a href="https://github.com/GSDion" target='blank' class="fa fa-github" > </a>
+            <a href="mailto:humphreydion@yahoo.com?subject=Hello!" target='blank' class="fa fa-yahoo" > </a>
         </div>
 
         <div className='Footer_nav_links'>
