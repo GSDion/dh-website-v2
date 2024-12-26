95243810f0c0deca52906f42c27536f4f596912e
GSDion <97761993+GSDion@users.noreply.github.com>
2024-12-26 17:01:09 -0500

    increase size of fa icons in footer.js
    - clean up Footer.css

--- src/frontend/Footer.js
+++ src/frontend/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
       <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top custom-copy-footer">
         <p class="col-md-4 mb-0 ">&copy; 2024 Dion Humphrey</p>
 
-        <a href="https://www.linkedin.com/in/dion-humphrey/" col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none target='blank' class="fa fa-linkedin"> </a>
-        <a href="https://github.com/GSDion" col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none target='blank' class="fa fa-github" > </a>
-        <a href="mailto:humphreydion@yahoo.com?subject=Hello!" col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none target='blank' class="fa fa-yahoo" > </a>
+        <a href="https://www.linkedin.com/in/dion-humphrey/" col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none target='blank' class="fa fa-linkedin fa-2x"> </a>
+        <a href="https://github.com/GSDion" col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none target='blank' class="fa fa-github fa-2x" > </a>
+        <a href="mailto:humphreydion@yahoo.com?subject=Hello!" col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none target='blank' class="fa fa-yahoo fa-2x" > </a>
 
         <ul class="nav col-md-4 justify-content-end custom-nav-links-footer">
           <Nav.Link href="/#about">About</Nav.Link>
