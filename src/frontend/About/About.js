2ba87287ea98ee889c835fefec5af3b148a0d32d
GSDion <97761993+GSDion@users.noreply.github.com>
2024-12-27 19:44:54 -0500

    Clean up About\ & Contact\
    - removed old resume
    - cleaned up index.css

--- src/frontend/About/About.js
+++ src/frontend/About/About.js
@@ -1,6 +1,4 @@
 import './About.css';
-//add some animations to the background (rainbow like intro?)
-
 
 function About() {
     
@@ -9,29 +7,20 @@ function About() {
       <div className =  'About' >
 
         <div className = 'About_title '>
-        <h1>About</h1>
+          <h1>About</h1>
        </div>
 
-      <div className = 'About_container'>
+        <div className = 'About_container'>
           <div className = 'About_paragraph'> 
             <p>Hello! My name is Dion. I'm a recent graduate from the University of Georgia, where
               I obtained a degree in Computer Science with a certifcate in Applied Data Science. At the University of Georgia, I learned about topics such as 
               cloud computing, web development, and machine learning. With this knowledge, I am eager to pursue a career in software development.
             </p>
-
             <p>🔭 I’m currently working on Tactical Emblem Hub, a website developed using Python, Flask, SQLite, and SQLAlchemy, which helps manage Fire Emblem characters, teams, and strategies.</p>
-
             <p>🌱 I’m currently learning Powershell Script and C#</p>
-
             <p>🎮 I'm interested in video games and 3D printing.</p>
-            
           </div>
-
-          {/* <div className = 'About_photo'> 
-            <img src={require('../Pictures/DH_portfolio.jpg')} alt="DH_portfolio" />
-          </div> */}
-      </div>
-        
+        </div>
       </div>
     );
 }  
--- src/frontend/Contact/Contact.js
+++ src/frontend/Contact/Contact.js
@@ -49,12 +49,13 @@ function Contact() {
       <div className = 'Contact'>
       
         <div className='Contact_title'>
-            <h1>Contact</h1>
+          <h1>Contact</h1>
         </div>
 
         <p className='Contact_email'>Email: humphreydion@yahoo.com</p>
         <div className='card custom-contact-card'>
         <form ref={form} onSubmit={sendEmail}>
+          
             <div className='form row'>
               <input type="hidden" name="contact_number" />
               <div className='form-group col-md-6 mb-3'>
@@ -75,9 +76,9 @@ function Contact() {
               <div className='form-group col-md-6 mb-3'>
                 <label for="email">Email</label>
                 <input type="text" id="email" className='form-control' name="email" placeholder="*Your email..." required ></input>
-              </div>
+            </div>
               
-              <div className="form-group col-md-6">
+            <div className="form-group col-md-6">
                 <input 
                   type="submit" 
                   value="Submit" 
@@ -88,33 +89,6 @@ function Contact() {
             </div>
           </form>
         </div>
-          
-          
-          {/* <div className='Contact_form_container'>
-
-          <div className='Contact_form_container_left'>
-            <input type="hidden" name="contact_number" />
-            <label for="name">Full Name</label>
-            <input type="text" id="name" name="fullname" placeholder="*Your name..." required ></input>
-
-            <label for="phone_number">Phone Number</label>
-            <input type="text" id="phone_number" name="phone_number" placeholder="Your Phone number..."></input>
-
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" placeholder="*Your email..." required ></input>
-          </div>
-
-
-          <div className='Contact_form_container_right'>
-            <label for="subject">Subject</label>
-            <textarea id="subject" name="subject" placeholder="*Write something..." required></textarea>
-          </div>
-
-          <input type="submit" value="Submit"></input>
-          </div> */}
-      
-      
-
       </div>
     );
 } 
